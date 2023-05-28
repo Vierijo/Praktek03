f8bb0495240ee5548744777eb608f788c89ebf3d
Vierijo <vierijosua732@gmail.com>
2023-05-28 08:11:24 +0800

    Instance Buat Atribut dan Panggilan Method

--- src/praktek03/SepedaAksi.java
+++ src/praktek03/SepedaAksi.java
@@ -1,6 +1,12 @@
 package praktek03;
 public class SepedaAksi {
     public static void main(String[] args) {
-        
+        Sepeda s = new Sepeda();
+
+        s.Merk = "Kupu - Kupu";
+        s.Warna = "Merah";
+        s.Harga =  1000;
+
+        s.cetakInfo();
     }
 }
