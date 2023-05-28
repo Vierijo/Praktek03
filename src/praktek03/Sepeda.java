dfee86ba904210831aea40a7a3eb4fa403b4173a
Vierijo <vierijosua732@gmail.com>
2023-05-28 08:04:46 +0800

    Buat Method cetakInfo()

--- src/praktek03/Sepeda.java
+++ src/praktek03/Sepeda.java
@@ -3,4 +3,10 @@ public class Sepeda {
     String Merk;
     String Warna;
     double Harga;
+    
+    void cetakInfo(){
+        System.out.println("Merk \t : " + Merk + "\n" +
+                           "Warna \t : " + Warna + "\n" +
+                           "Harga \t : " + Harga);
+    }
 }
