d092d2f8c5ed331cc03ea158f95c09a329cc4110
Vierijo <vierijosua732@gmail.com>
2023-05-28 08:12:56 +0800

    Buat Getter & Setter

--- src/praktek03/Sepeda.java
+++ src/praktek03/Sepeda.java
@@ -2,5 +2,37 @@ package praktek03;
 public class Sepeda {
     private String Merk;
     private String Warna;
-    private double Harga;  
+    private double Harga;
+    
+    void cetakInfo(){
+        System.out.println("Merk \t : " + Merk + "\n" +
+                           "Warna \t : " + Warna + "\n" +
+                           "Harga \t : " + Harga);
+    }
+
+    public String getMerk() {
+        return Merk;
+    }
+
+    public void setMerk(String Merk) {
+        this.Merk = Merk;
+    }
+
+    public String getWarna() {
+        return Warna;
+    }
+
+    public void setWarna(String Warna) {
+        this.Warna = Warna;
+    }
+
+    public double getHarga() {
+        return Harga;
+    }
+
+    public void setHarga(double Harga) {
+        this.Harga = Harga;
+    }
+    
+    
 }
