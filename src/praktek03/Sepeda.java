7063f0a4b015e48d65b3b1ef5cf6bc2813ff109a
Vierijo <vierijosua732@gmail.com>
2023-05-28 08:12:24 +0800

    Set Atribut Private

--- src/praktek03/Sepeda.java
+++ src/praktek03/Sepeda.java
@@ -2,37 +2,5 @@ package praktek03;
 public class Sepeda {
     private String Merk;
     private String Warna;
-    private double Harga;
-    
-    void cetakInfo(){
-        System.out.println("Merk \t : " + Merk + "\n" +
-                           "Warna \t : " + Warna + "\n" +
-                           "Harga \t : " + Harga);
-    }
-
-    public String getMerk() {
-        return Merk;
-    }
-
-    public void setMerk(String Merk) {
-        this.Merk = Merk;
-    }
-
-    public String getWarna() {
-        return Warna;
-    }
-
-    public void setWarna(String Warna) {
-        this.Warna = Warna;
-    }
-
-    public double getHarga() {
-        return Harga;
-    }
-
-    public void setHarga(double Harga) {
-        this.Harga = Harga;
-    }
-    
-    
+    private double Harga;  
 }
